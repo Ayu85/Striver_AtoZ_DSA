860945ef14b2f75e205d96a99570eb8706219bb7
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-06 04:31:12 +0000

    1 done

--- Arrays/largestElement.java
+++ Arrays/largestElement.java
@@ -2,15 +2,17 @@ package Arrays;
 
 public class largestElement {
     static int findLargest(int[] arr, int n) {
-        int max = arr[0];
-        for (int i = 0; i < n; i++) {
-            if (arr[i] > max)
-                max = arr[i];
+        int max = Integer.MIN_VALUE;
+        for (int x : arr) {
+            if (x > max) {
+                max = x;
+            }
         }
         return max;
     }
+
     public static void main(String[] args) {
-        int[] arr={2,0,3,-5,8};
+        int[] arr = { 2, 0, 3, -5, 8, 45, 8889, 2 };
         System.out.println(findLargest(arr, arr.length));
     }
 }
