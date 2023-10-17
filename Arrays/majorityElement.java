c19004e429368c76c182d21f960b2a1874857ece
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-17 05:28:59 +0000

    today 2 done

--- Arrays/majorityElement.java
+++ Arrays/majorityElement.java
@@ -16,7 +16,9 @@ public class majorityElement {
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 3, 3, 3, 1, 2, 3 };
+        int[] arr = { 2, 3, 3, 3, 1, 2, 3, 3, 2, 1, 3, 2, 3, 1, 54, 46, 341, 63, 46, 6341, 3, 413, 54163, 453, 564,
+                64, 31, 3, 1, 21, 3, 41, 3, 1, 32, 1, 32, 41, 3, 564, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+                3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3 };
         System.out.println(getElement(arr, arr.length));
     }
 }
