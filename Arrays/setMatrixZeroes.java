0277ec90960d3be1a042591b4a1f6096d10d7b6f
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-19 07:29:34 +0000

    todays done

--- Arrays/setMatrixZeroes.java
+++ Arrays/setMatrixZeroes.java
@@ -39,7 +39,16 @@ public class setMatrixZeroes {
 
     public static void main(String[] args) {
         int[][] arr = { { 1, 1, 1, 1 }, { 1, 0, 0, 1 }, { 1, 1, 0, 1 }, { 1, 1, 1, 1 } };
+        System.out.println("Matrix Before Setting Zeroes: ");
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(arr[i][j] + "   ");
+            }
+            System.out.println();
+        }
         setZeroes(arr, 4, 4);
+        System.out.println("Matrix After Setting Zeroes: ");
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 System.out.print(arr[i][j] + "   ");
