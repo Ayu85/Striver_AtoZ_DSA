89518705bcf72247e185c9c6e6512f94e8dfb67d
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-22 06:51:37 +0000

    better approach

--- Arrays/countSubarraysSumEqualsK.java
+++ Arrays/countSubarraysSumEqualsK.java
@@ -3,12 +3,21 @@ package Arrays;
 public class countSubarraysSumEqualsK {
     static int countSubarrays(int[] arr, int n, int target) {
         int count = 0;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n; j++) {
-                int sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += arr[k];
-                }
+        // for (int i = 0; i < n - 1; i++) {
+        // for (int j = 0; j < n; j++) {
+        // int sum = 0;
+        // for (int k = i; k <= j; k++) {
+        // sum += arr[k];
+        // }
+        // if (sum == target)
+        // count++;
+        // }
+        // }
+        // return count;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
                 if (sum == target)
                     count++;
             }
