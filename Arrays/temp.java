84a44bc52986e5ceebaca4711d5b9c6dd501d8a5
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-23 15:06:35 +0000

    todays commit new

---
+++ Arrays/temp.java
@@ -0,0 +1,34 @@
+package Arrays;
+
+public class temp{
+    static int countSubarrays(int[] arr, int n, int target) {
+        int count = 0;
+        // for (int i = 0; i < n - 1; i++) {
+        // for (int j = 0; j < n; j++) {
+        // int sum = 0;
+        // for (int k = i; k <= j; k++) {
+        // sum += arr[k];
+        // }
+        // if (sum == target)
+        // count++;
+        // }
+        // }
+        // return count;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += arr[j];
+                if (sum == target)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, -3, 1, 1, 1, 4, 2, -3 };
+        System.out.println(countSubarrays(arr, arr.length, 3));
+        System.out.println("commit");
+
+    }
+}
