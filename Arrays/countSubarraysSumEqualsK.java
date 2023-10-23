51a3bd9889213c72a4772f47c30b5b44ed3c6fab
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-23 15:03:55 +0000

    todays commit

--- Arrays/countSubarraysSumEqualsK.java
+++ Arrays/countSubarraysSumEqualsK.java
@@ -28,6 +28,7 @@ public class countSubarraysSumEqualsK {
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, -3, 1, 1, 1, 4, 2, -3 };
         System.out.println(countSubarrays(arr, arr.length, 3));
+        System.out.println("commit");
 
     }
 }
