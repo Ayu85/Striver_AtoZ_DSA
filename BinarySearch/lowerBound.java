6a7d1cbf3b38a88db4ed4100a960faf64b4f2b11
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-25 06:40:56 +0000

    brute force way done

--- BinarySearch/lowerBound.java
+++ BinarySearch/lowerBound.java
@@ -1,3 +1,15 @@
 public class lowerBound {
-    
+    static int getaLowerBound(int[] arr, int len, int x) {
+        for (int i = 0; i < len; i++) {
+            if (arr[i] >= x)
+                return i;
+        }
+        return 0;
+
+    }
+
+public static void main(String[] args) {
+    int[] arr={3,4,8,15,19};
+    System.out.println(getaLowerBound(arr, arr.length, 15));
+}
 }
