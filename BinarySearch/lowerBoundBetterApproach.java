ea7effecb0cf3459f3f205e5f4b0af5f0cdd277a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-25 07:08:42 +0000

    recursiveApproach

--- BinarySearch/lowerBoundBetterApproach.java
+++ BinarySearch/lowerBoundBetterApproach.java
@@ -1,5 +1,19 @@
 public class lowerBoundBetterApproach {
-    static int getLower(int[] arr,int n,int x){
-        
+    static int getLower(int[] arr, int low, int high, int target) {
+        int ans = high + 1;
+        if (low > high)
+            return ans;
+        int mid = (low + high) / 2;
+        if (arr[mid] >= target) {
+            ans = mid;
+            return getLower(arr, low, mid - 1, target);
+        } else {
+            return getLower(arr, mid + 1, high, target);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 3, 5, 8, 8, 10, 10, 11 };
+        System.out.println(getLower(arr, 0, arr.length - 1, 1));
     }
 }
