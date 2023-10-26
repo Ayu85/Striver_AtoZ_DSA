21ee2c3e91ebcc6dfe699c5100afbe52fba86225
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-26 07:07:21 +0000

    optimal way done

--- BinarySearch/firstLastOccurrenceOptimal.java
+++ BinarySearch/firstLastOccurrenceOptimal.java
@@ -12,6 +12,8 @@ public class firstLastOccurrenceOptimal {
                 high = mid - 1;
             } else if (arr[mid] < x)
                 low = mid + 1;
+            else
+                high = mid - 1;
 
         }
         // to find the last occurrence
@@ -23,21 +25,23 @@ public class firstLastOccurrenceOptimal {
         int low = 0;
         int high = n - 1;
         int last = -1;
-        while (low < high) {
+        while (low <= high) {
             int mid = (low + high) / 2;
             if (arr[mid] == x) {
                 last = mid;
                 low = mid + 1;
             } else if (arr[mid] > x)
                 high = mid - 1;
+            else
+                low = mid + 1;
 
         }
         return last;
     }
 
     public static void main(String[] args) {
-        int[] arr = { 2, 8, 8, 8, 8, 8, 11, 13 };
-        System.out.println(getFirstOccurrance(arr, arr.length, 8));
-        System.out.println(getlastOccurrence(arr, arr.length, 8));
+        int[] arr = { 2, 8, 8, 8, 8, 8, 11, 11, 11 };
+        System.out.println(getFirstOccurrance(arr, arr.length, 11));
+        System.out.println(getlastOccurrence(arr, arr.length, 11));
     }
 }
