856264a990d2e5cda6a3aea0d7c4cb0a3eea8f2a
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-26 06:01:11 +0000

    first dsa problem done

---
+++ BinarySearch/firstLastOccurrance.java
@@ -0,0 +1,24 @@
+public class firstLastOccurrance {
+    static int[] getOccurrances(int[] arr, int n, int x) {
+        int first = -1;
+        int last = -1;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x) {
+                if (first == -1) {
+                    first = i;
+                    last = i;
+
+                } else
+                    last = i;
+            }
+        }
+        return new int[] { first, last };
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 5, 8, 8, 8, 9, 10 };
+        int res[] = getOccurrances(arr, arr.length, 8);
+        for (int x : res)
+            System.out.print(x+"   ");
+    }
+}
