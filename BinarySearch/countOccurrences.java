340f8f32c6bfb91db5353044aefd684d1e0f8490
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-27 07:58:03 +0000

    1 dsa done

--- BinarySearch/countOccurrences.java
+++ BinarySearch/countOccurrences.java
@@ -1,25 +1,44 @@
-public class countOccurrences{
-    static int firstOccr(int[] arr,int n,int x)
-    {
-        int ans=0;
-		int low=0;
-		int high=n-1;
-		while(low<=high) {
-			int mid=(low+high)/2;
-			if(arr[mid]==x)
-			{
-				ans=mid;
-				high=mid-1;
-			}
-			else if(arr[mid]>x)
-					high=mid-1;
+public class countOccurrences {
+	static int firstOcc(int[] arr, int n, int x) {
+		int ans = 0;
+		int low = 0;
+		int high = n - 1;
+		while (low <= high) {
+			int mid = (low + high) / 2;
+			if (arr[mid] == x) {
+				ans = mid;
+				high = mid - 1;
+			} else if (arr[mid] > x)
+				high = mid - 1;
 			else
-				low=mid+1;
+				low = mid + 1;
 		}
 		return ans;
-    }
-	static int 
+	}
+
+	static int lastOcc(int[] arr,int n,int x){
+		int ans=0;
+		int low=0;
+		int high=n-1;
+		while(low<=high){
+			int mid=(low+high)/2;
+			if(arr[mid]==x)
+			{	ans=mid;
+				low=mid+1;
+			}
+			else if(arr[mid]>x)
+				high=mid-1;
+
+			else
+			low=mid+1;
+		}
+		return ans;
+	}
+
 	public static void main(String[] args) {
-		
+		int[] arr = { 2, 4, 5, 6, 8, 8, 8, 9, 10 };
+		int first = firstOcc(arr, arr.length, 9);
+		int last = lastOcc(arr, arr.length, 9);
+		System.out.println(last-first+1);
 	}
 }
