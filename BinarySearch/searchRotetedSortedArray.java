75e0f0c8ceb2a08b5325bbccb6f6e63a9d89b434
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-28 07:48:51 +0000

    50% test case passed

--- BinarySearch/searchRotetedSortedArray.java
+++ BinarySearch/searchRotetedSortedArray.java
@@ -8,16 +8,23 @@ public class searchRotatedSortedArray {
                 return mid;
             // check for the sorted part(left part)
             else if (arr[low] <= arr[mid]) {
-
-            } 
-            //check sorted right part
+                if (target <= arr[low] && target <= arr[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            // check sorted right part
             else {
-
+                if (target >= arr[mid] && target <= arr[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
             }
         }
     }
 
     public static void main(String[] args) {
-
+        int[] arr = { 7, 8, 9, 1, 2, 3, 4, 5, 6 };
+        System.out.println(getELement(arr, arr.length, 2));
     }
 }
