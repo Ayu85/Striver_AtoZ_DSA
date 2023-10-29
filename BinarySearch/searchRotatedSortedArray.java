3f7a2c48c00240d4c591d2eb85035e41f6263fe9
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-29 06:28:43 +0000

    100% test cases passed

--- BinarySearch/searchRotatedSortedArray.java
+++ BinarySearch/searchRotatedSortedArray.java
@@ -26,6 +26,6 @@ public class searchRotatedSortedArray {
 
     public static void main(String[] args) {
         int[] arr = { 7, 8, 9, 1, 2, 3, 4, 5, 6 };
-        System.out.println(getELement(arr, arr.length, 1));
+        System.out.println(getELement(arr, arr.length, 6));
     }
 }
