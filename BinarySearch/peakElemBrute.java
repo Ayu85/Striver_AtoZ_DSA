203d25f7671b9ba4553847fe1e884227a6bdef84
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-10-31 18:17:58 +0000

    all test cases passed

--- BinarySearch/peakElemBrute.java
+++ BinarySearch/peakElemBrute.java
@@ -10,7 +10,7 @@ public static void main(String[] args) {
     int[] arr={1,2,3,4,5,6,7,8,5,1};
     int [] arr2={1,2,1,3,5,6,4};
     int[] arr3={1,2,3,4,5};
-    int[] arr4={5,4,3,2,1};
+    int[] arr4={5,4,2,3,1};
     System.out.println(getElm(arr3,arr3.length));
 }
 }
