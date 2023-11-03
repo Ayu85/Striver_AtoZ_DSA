70b35a32cbeddaf99ea5fdd0fe10b6062ecdb814
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-03 07:04:15 +0000

    brute force tech.

--- BinarySearch/nthRoot.java
+++ BinarySearch/nthRoot.java
@@ -1,12 +1,12 @@
 public class nthRoot {
     static int getRoot(int n,int m){
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=m/2;i++){
             if(Math.pow(i, n)==m)
             return n;
         }
         return -1;
     }
     public static void main(String[] args) {
-        System.out.println(getRoot(4,256));
+        System.out.println(getRoot(3,27));
     }
 }
