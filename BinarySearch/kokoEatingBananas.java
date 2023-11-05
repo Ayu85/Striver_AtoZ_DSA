f4268eb267aa636f660e4e8c09a95151aca0a8ed
Aayush Singh <103013928+Ayu85@users.noreply.github.com>
2023-11-05 06:29:55 +0000

    50% test cases passed

--- BinarySearch/kokoEatingBananas.java
+++ BinarySearch/kokoEatingBananas.java
@@ -1,15 +1,22 @@
 public class kokoEatingBananas {
     static int getHours(int[] piles, int h, int n) {
         for (int i = 1; i <= h; i++) {
-            int ans = 0;
-            int hours = i;
+            float ans = 0;
+            float hours = i;
             for (int j = 0; j < n; j++) {
                 ans += Math.ceil(piles[j] / hours);
+                System.out.print((piles[j] / hours)+" ");
             }
+            System.out.println();
             if (ans == h)
-                return hours;
+                return i;
         }
         return -1;
     }
-    
+
+    public static void main(String[] args) {
+        int[] arr = { 3, 6, 7, 11 };
+        System.out.println(getHours(arr, 8, arr.length));
+    }
+
 }
